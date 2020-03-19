75e22632b6c4e1f41d8254701b7dbcfea29c6c65
User <User@PC-Lite>
2020-03-19 15:11:56 +0300

    Added comments to Cryptor class

--- SWayfarer's Lib II/src/ru/swayfarer/swl2/crypto/Cryptor.java
+++ SWayfarer's Lib II/src/ru/swayfarer/swl2/crypto/Cryptor.java
@@ -1,20 +1,41 @@
 package ru.swayfarer.swl2.crypto;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+import ru.swayfarer.swl2.collections.CollectionsSWL;
 import ru.swayfarer.swl2.logger.ILogger;
 import ru.swayfarer.swl2.logger.LoggingManager;
+import ru.swayfarer.swl2.markers.InternalElement;
+import ru.swayfarer.swl2.string.StringUtils;
 
+/**
+ * Шифровальщик 
+ * @author swayfarer
+ *
+ */
 @SuppressWarnings("unchecked")
 public class Cryptor {
 
+	/** Логгер */
+	@InternalElement
 	public ILogger logger = LoggingManager.getLogger();
+	
+	/** {@link Cipher} для расшифровки */
+	@InternalElement
 	public Cipher decryptCypher;
+	
+	/** {@link Cipher} для pашифровки */
+	@InternalElement
 	public Cipher encryptCipher;
+	
+	/** Ключ для шифровки */
+	@InternalElement
 	public SecretKey key;
 	
+	/** Загрузить, зная тип шифрования и ключ */
 	public <T extends Cryptor> T load(String cryptType, byte[] keyBytes) 
 	{
 		logger.safe(() -> {
@@ -29,7 +50,7 @@ public class Cryptor {
 				System.out.println(algoritmName);
 			}
 			
-			this.key = new SecretKeySpec(keyBytes, algoritmName);
+			this.key = getKey(algoritmName, keyBytes);
 			decryptCypher = Cipher.getInstance(cryptType);
 			encryptCipher = Cipher.getInstance(cryptType);
 			
@@ -41,18 +62,72 @@ public class Cryptor {
 		return (T) this;
 	}
 	
+	/** Загрузить, зная тип шифрования и ключ */
 	public <T extends Cryptor> T load(String cryptType, String key) 
 	{
-		return load(cryptType, key.getBytes());
+		return load(cryptType, key == null ? null : key.getBytes());
 	}
 	
+	/** Получить ключ */
+	public SecretKey getKey(String keyType, byte[] keyBytes)
+	{
+		if (StringUtils.isEmpty(keyType) || CollectionsSWL.isNullOrEmpty(keyBytes))
+		{
+			try
+			{
+				KeyGenerator keyGenerator = KeyGenerator.getInstance(keyType);
+				return keyGenerator.generateKey();
+			}
+			catch (Throwable e)
+			{
+				logger.error(e, "Error while creating crypto key of type", keyType);
+			}
+			
+			return null;
+		}
+		
+		return new SecretKeySpec(keyBytes, keyType);
+	}
+	
+	/** Получить декриптор */
 	public Cipher getDecryptCypher()
 	{
 		return decryptCypher;
 	}
 	
+	/** Получить криптор */
 	public Cipher getEncryptCipher()
 	{
 		return encryptCipher;
+	}
+	
+	/** Зашифровать байты */
+	public byte[] encrypt(byte[] bytes)
+	{
+		try
+		{
+			return getEncryptCipher().doFinal(bytes);
+		}
+		catch (Throwable e)
+		{
+			logger.error(e, "Error while encrypting bytes");
+		}
+		
+		return null;
+	}
+	
+	/** Расшифровать байты */
+	public byte[] decrypt(byte[] bytes)
+	{
+		try
+		{
+			return getDecryptCypher().doFinal(bytes);
+		}
+		catch (Throwable e)
+		{
+			logger.error(e, "Error while encrypting bytes");
+		}
+		
+		return null;
 	}
 }
